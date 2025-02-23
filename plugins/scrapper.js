4db8a6820a4c394a55decbfba2843f578721b650
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-02-23 19:46:24 +0530

    Update scrapper.js

--- plugins/scrapper.js
+++ plugins/scrapper.js
@@ -10,21 +10,6 @@ const {
 
 
 plugin({
-    pattern: 'google',
-    fromMe: mode,
-    desc: 'search on Google',
-    react: "🙃",
-    type: "search"
-}, async (message, match) => {
-    if (!match) return message.send('_give me some query_');
-    const { result, status } = await getJson(`${config.BASE_URL}api/search/pinterest?text=${match}&apikey=${config.INRL_KEY}`);
-    if (!status) return await message.send(`API key limit exceeded. Get a new API key at ${config.BASE_URL}api/signup. Set var INRL_KEY: your_api_key`);
-    await Promise.all(result.map(async (item) => {
-        await message.send(`*Title:* ${item.title}\n*Link:* ${item.link}\n*Snippet:* ${item.snippet}\n`);
-    }));
-});
-
-plugin({
     pattern: 'ringtone',
     fromMe: mode,
     desc: 'download ringtone',
